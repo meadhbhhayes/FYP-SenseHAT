e339baa28d12ccaa4ed28471cfda269294df90b8
Meadbh <meadhbh.hayes@mycit.ie>
2020-03-10 16:53:53 +0000

    Basic Reading Angles Loop

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from sense_hat import SenseHat
+import time
 
 #naming variable
 sense = SenseHat() 
 
 def main():
-    sense.show_message("Hello World")
+    while True:
+        results = sense.get_orientation_degrees()
+        yaw = results['yaw']
+        pitch = results['pitch']
+        roll = results['roll']
+
+
+        sense.show_message("YAW: " + yaw)
+        sense.show_message("PITCH: " + pitch)
+        sense.show_message("ROLL: " + roll)
+
+        time.sleep(0.5)
     return
 
 if __name__ == "__main__":
